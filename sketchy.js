257998cb33136a26146b26a5646a0bdc6a9e5d51
Kyle Krafka <kyle.krafka@gmail.com>
2013-04-05 13:50:24 -0400

    Add method to convert SVG data to point arrays
    Simplify.js works with point arrays, so this method will
    convert Raphael SketchPad SVG JSON to the correct
    format.  Test case is included.

--- sketchy.js
+++ sketchy.js
@@ -22,6 +22,29 @@
   };
 
   /* Kyle's Algorithms */
+  // Takes in SVG data (from Raphael SketchPad) and outputs an array of paths,
+  // each of which is an array of points in {x: Number, y: Number} format.
+  // This is useful for preprocessing for Simplify.js or any other algorithm
+  // operating on simple paths.
+  Sketchy.convertSVGtoPointArrays = function(json) {
+    var i, splitPath, j, point, paths;
+
+    paths = [];
+    json = JSON.parse(json);
+    for(i=0; i<json.length; i++) {
+      // Take the SVG data for the current path, cut off the M at the
+      // beginning, and then explode the string into an array, split at
+      // the "L" character.  This is the format from Raphael SketchPad
+      splitPath = json[i].path.slice(1).split("L");
+      paths[i] = [];
+      for(j=0; j<splitPath.length; j++) {
+        point = splitPath[j].split(",");
+        paths[i][j] = {x: point[0], y: point[1]};
+      }
+    }
+    return paths;
+  };
+
   Sketchy.shapeContextMatch = function(shape1, shape2) {
     return 0;
   };

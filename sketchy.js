e32c4167a69e93bcf9fd66241637ac245fadd73b
Kyle Krafka <kyle.krafka@gmail.com>
2013-04-05 13:46:06 -0400

    Namespace the code and add documentation
    This will put chosen public properties under the object "Sketchy"
    and protect other variables.

--- sketchy.js
+++ sketchy.js
@@ -1,20 +1,33 @@
-// Sketchy.js
-(function() {
-  var randomShapeMatch, shapeContextMatch, hausdorffDistance;
+//////////////////////////////////////////
+//              Sketchy.js              //
+//////////////////////////////////////////
+//
+// JavaScript Shape Matching
+// Works best with Raphael SketchPad
+// Development started March 2013
+//
+
+// Immediately invoke an annonymous function to keep the global scope clean.
+// Parameters:
+// - global: will be the global object; called with "this" from global scope
+// - undefined: keeps "undefined" undefined; no 2nd arg will make it undefined
+(function(global, undefined) {
+  // Namespace everything
+  global.Sketchy = {};
 
   /* Jordan's Algorithms */
   // Test function for front-end application development
-  randomShapeMatch = function(shape1, shape2) {
+  Sketchy.randomShapeMatch = function(shape1, shape2) {
     return Math.random();
   };
 
   /* Kyle's Algorithms */
-  shapeContextMatch = function(shape1, shape2) {
+  Sketchy.shapeContextMatch = function(shape1, shape2) {
     return 0;
   };
 
   /* Betim's Algorithms */
-  hausdorffDistance = function(shape1, shape2) {
+  Sketchy.hausdorffDistance = function(shape1, shape2) {
   	return 0;
   };
-}).call(this);
+})(this);

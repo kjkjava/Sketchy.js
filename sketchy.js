87bba88700afa1b7fb83f1bb06770e1cdfedf024
Kyle Krafka <kyle.krafka@gmail.com>
2013-04-27 00:40:40 -0400

    Finish shapeContextMatch and include Hungarian algorithm
    It seems to work to some extent!  Certainly not perfected,
    and the return value is not in the range 0 to 100, but rather
    an error value is returned (e.g. 0 is a perfect match).  More
    testing and tweaks will be required, but everything seems to
    right in terms of the shape context algorithm I was reading about.
    Also, the Hungarian algorithm piece was a big enough task to
    tackle that I made it its own project, repository and all.  As not to
    require any additional files just yet, I pasted it in here.  That was
    a beast of its own.

--- sketchy.js
+++ sketchy.js
@@ -97,10 +97,19 @@
   Sketchy.shapeContextMatch = function(shape1, shape2) {
     var pointsPerShape = 50, // constant
         points1, points2,
-        distanceMatrix, distanceTotal, distanceMean, distanceBinSmallest = 0.125, distanceBinCount = 5, distanceBins,
-        angleMatrix, angleBinCount = 12, angleBins,
+
+        distanceBinSmallest = 0.125, distanceBinCount = 5,
+        distanceMatrix1, distanceTotal1, distanceMean1, distanceBins1,
+        distanceMatrix2, distanceTotal2, distanceMean2, distanceBins2,
+
+        angleBinCount = 12,
+        angleMatrix1, angleBins1,
+        angleMatrix2, angleBins2,
+
         costMatrix,
-        distanceBinNumber, angleBinNumber, ksum,
+        distanceBinNumber1, angleBinNumber1,
+        distanceBinNumber2, angleBinNumber2,
+        ksum, compare,
         i, j, k;
 
     // Scatter points around each of the paths.  The algorithm
@@ -114,36 +123,48 @@
     points2 = Sketchy.scatterPoints(Sketchy.convertSVGtoPointArrays(shape2), pointsPerShape);
 
     // Create a square 2D array and initialize it with 0s in the diagonal
-    distanceMatrix = [];
+    distanceMatrix1 = [];
+    distanceMatrix2 = [];
     for(i=0; i<pointsPerShape; i++) {
-      distanceMatrix[i] = [];
-      distanceMatrix[i][i] = 0;
+      distanceMatrix1[i] = [];
+      distanceMatrix1[i][i] = 0;
+      distanceMatrix2[i] = [];
+      distanceMatrix2[i][i] = 0;
     }
 
     // Go through the upper triangle of the matrix, computing the distance, mirroring to the lower
-    distanceTotal = 0;
+    distanceTotal1 = 0;
+    distanceTotal2 = 0;
     for(i=0; i<pointsPerShape-1; i++) {
       for(j=i+1; j<pointsPerShape; j++) {
-        distanceMatrix[i][j] = distanceMatrix[j][i] = Sketchy.euclideanDistance(points1[i].x, points1[i].y, points2[j].x, points2[j].y);
-        distanceTotal += distanceMatrix[i][j];
+        distanceMatrix1[i][j] = distanceMatrix1[j][i] = Sketchy.euclideanDistance(points1[i].x, points1[i].y, points1[j].x, points1[j].y);
+        distanceMatrix2[i][j] = distanceMatrix2[j][i] = Sketchy.euclideanDistance(points2[i].x, points2[i].y, points2[j].x, points2[j].y);
+        distanceTotal1 += distanceMatrix1[i][j];
+        distanceTotal2 += distanceMatrix2[i][j];
       }
     }
-    distanceTotal *= 2; // 0s were already summed in, we just need to double it since we only went through the upper triangle
-    distanceMean = distanceTotal/Math.pow(pointsPerShape,2);
+    distanceTotal1 *= 2; // 0s were already summed in, we just need to double it since we only went through the upper triangle
+    distanceTotal2 *= 2;
+    distanceMean1 = distanceTotal1/Math.pow(pointsPerShape,2);
+    distanceMean2 = distanceTotal2/Math.pow(pointsPerShape,2);
 
     // Normalize by the mean distance
     for(i=0; i<pointsPerShape; i++) {
       for(j=0; j<pointsPerShape; j++) {
-        distanceMatrix[i][j] /= distanceMean;
+        distanceMatrix1[i][j] /= distanceMean1;
+        distanceMatrix2[i][j] /= distanceMean2;
       }
     }
 
     // Initialize the distance bins with all 0s
-    distanceBins = [];
+    distanceBins1 = [];
+    distanceBins2 = [];
     for(i=0; i<pointsPerShape; i++) {
-      distanceBins[i] = [];
+      distanceBins1[i] = [];
+      distanceBins2[i] = [];
       for(j=0; j<pointsPerShape; j++) {
-        distanceBins[i][j] = 0;
+        distanceBins1[i][j] = 0;
+        distanceBins2[i][j] = 0;
       }
     }
 
@@ -155,9 +176,13 @@
     for(k=0; k<distanceBinCount; k++) {
       for(i=0; i<pointsPerShape; i++) {
         for(j=0; j<pointsPerShape; j++) {
-          if(distanceMatrix[i][j] < distanceBinSmallest) {
-            distanceBins[i][j]++;
-            distanceBins[j][i]++;
+          if(distanceMatrix1[i][j] < distanceBinSmallest) {
+            distanceBins1[i][j]++;
+            distanceBins1[j][i]++;
+          }
+          if(distanceMatrix2[i][j] < distanceBinSmallest) {
+            distanceBins2[i][j]++;
+            distanceBins2[j][i]++;
           }
         }
       }
@@ -167,36 +192,43 @@
     // Angles //
 
     // Create a square 2D array and initialize it with 0s in the diagonal
-    angleMatrix = [];
+    angleMatrix1 = [];
+    angleMatrix2 = [];
     for(i=0; i<pointsPerShape; i++) {
-      angleMatrix[i] = [];
-      angleMatrix[i][i] = 0;
+      angleMatrix1[i] = [];
+      angleMatrix2[i] = [];
+      angleMatrix1[i][i] = 0;
+      angleMatrix2[i][i] = 0;
     }
 
     // Compute the angle matrix, much like the distance matrix
     for(i=0; i<pointsPerShape-1; i++) {
       for(j=i+1; j<pointsPerShape; j++) {
         // Adding 2pi and modding by 2pi changes the -pi to pi range to a 0 to 2pi range
-        angleMatrix[i][j] = angleMatrix[j][i] = (Math.atan2(points2[j].y - points1[i].y, points2[j].x - points1[i].x) + 2*Math.PI) % (2*Math.PI);
+        angleMatrix1[i][j] = angleMatrix1[j][i] = (Math.atan2(points1[j].y - points1[i].y, points1[j].x - points1[i].x) + 2*Math.PI) % (2*Math.PI);
+        angleMatrix2[i][j] = angleMatrix2[j][i] = (Math.atan2(points2[j].y - points2[i].y, points2[j].x - points2[i].x) + 2*Math.PI) % (2*Math.PI);
       }
     }
 
     // Initialize the angle bins
-    angleBins = [];
+    angleBins1 = [];
+    angleBins2 = [];
     for(i=0; i<pointsPerShape; i++) {
-      angleBins[i] = [];
+      angleBins1[i] = [];
+      angleBins2[i] = [];
     }
 
     // Compute the angle bins
     for(i=0; i<pointsPerShape; i++) {
       for(j=0; j<pointsPerShape; j++) {
-        angleBins[i][j] = angleBins[j][i] = 1+Math.floor(angleMatrix[i][j]/(2*Math.PI/angleBinCount));
+        angleBins1[i][j] = angleBins1[j][i] = 1+Math.floor(angleMatrix1[i][j]/(2*Math.PI/angleBinCount));
+        angleBins2[i][j] = angleBins2[j][i] = 1+Math.floor(angleMatrix2[i][j]/(2*Math.PI/angleBinCount));
       }
     }
 
     // Cost Matrix //
     // Compute the cost matrix.  This skips the combined histogram for the sake
-    // of efficiency.
+    // of efficiency.  TODO: make more efficient by only calculating upper triangle
     costMatrix = [];
     for(i=0; i<pointsPerShape; i++) {
       costMatrix[i] = [];
@@ -206,17 +238,17 @@
         for(logr=1; logr<=distanceBinCount; logr++) {
           for(theta=1; theta<=angleBinCount; theta++) {
             // calculate hik and hjk
-            hik = Sketchy.shapeContextHistogram(i, distanceBinNumber, angleBinNumber, distanceBins, angleBins);
-            hjk = Sketchy.shapeContextHistogram(j, distanceBinNumber, angleBinNumber, distanceBins, angleBins);
-
-            ksum += Math.pow(hik-hjk,2) / (hik + hjk);
+            hik = Sketchy.shapeContextHistogram(i, logr, theta, distanceBins1, angleBins1);
+            hjk = Sketchy.shapeContextHistogram(j, logr, theta, distanceBins2, angleBins2);
+            compare = (hik + hjk === 0) ? 0 : (Math.pow(hik-hjk,2) / (hik + hjk));
+            ksum += compare;
           }
         }
         costMatrix[i][j] = 1/2 * ksum;
       }
     }
-
-    return 0;
+    console.log(costMatrix);
+    return Sketchy.hungarian(costMatrix);
   };
 
   // Sums up the number of points (relative to point pointIndex) in a particular
@@ -457,4 +489,448 @@
   Sketchy.bottleneckDistance = function(shape1, shape2) {
   	return 0;
   };
+
+
+
+
+
+
+
+
+
+  // The Hungarian Algorithm (which I will also publish as a standalone library,
+  // Hungarian.js).  It's used in shapeContextMatch.
+  // TODO: Consider making this a required include?  -Kyle
+  // Hungarian Algorithm
+
+  Sketchy.hungarian = (function() {
+    // Takes in a 2D array and finds the largest element
+    // This is used in the Hungarian algorithm if the user chooses "max"
+    // (indicating their matrix values represent profit) so that cost values
+    // are subtracted from the largest value.
+    function findLargest(matrix) {
+      var i, j, largest = Number.MIN_VALUE;
+      for(i=0; i<matrix.length; i++) {
+        for(j=0; j<matrix[i].length; j++) {
+          if(matrix[i][j] > largest) {
+            largest = matrix[i][j];
+          }
+        }
+      }
+      return largest;
+    }
+
+    // Copies all elements of a 2D array to a new array
+    function copyOf(original) {
+      var i, j,
+          copy = [];
+
+      for(i=0; i<original.length; i++) {
+        copy[i] = [];
+        for(j=0; j<original[i].length; j++) {
+          copy[i][j] = original[i][j];
+        }
+      }
+
+      return copy;
+    }
+
+    // sumType is optional, but if it exists and is "max," the costs will
+    // be treated as profits
+    function hgAlgorithm(matrix, sumType) {
+      var cost, maxWeight, i, j,
+        mask = [], // the mask array: [matrix.length] x [matrix[0].length]
+        rowCover = [], // the row covering vector: [matrix.length]
+        colCover = [], // the column covering vector: [matrix[0].length]
+        zero_RC = [0,0], // position of last zero from Step 4: [2]
+        path = [], // [matrix.length * matrix[0].length + 2] x [2]
+        step = 1,
+        done = false,
+        assignment = []; // [matrix.length] x [2]
+
+
+      // Create the cost matrix
+      cost = copyOf(matrix);
+
+      if(sumType && sumType.toLowerCase() === "max") {
+        maxWeight = findLargest(cost);
+        for(i=0; i<cost.length; i++) {
+          for(j=0; j<cost[i].length; j++) {
+            cost[i][j] = maxWeight - cost[i][j];
+          }
+        }
+      }
+
+      // Initialize the 1D arrays with zeros
+      for(i=0; i<matrix.length; i++) {
+        rowCover[i] = 0;
+      }
+      for(j=0; j<matrix[0].length; j++) {
+        colCover[j] = 0;
+      }
+
+      // Initialize the inside arrays to make 2D arrays
+      // Fill with zeros
+      for(i=0; i<matrix.length; i++) {
+        mask[i] = [];
+        for(j=0; j<matrix[0].length; j++) {
+          mask[i][j] = 0;
+        }
+        assignment[i] = [0,0];
+      }
+      for(i=0; i<(matrix.length * matrix[0].length + 2); i++) {
+        path[i] = [];
+      }
+
+      // Matrix execution loop
+      while(!done) {
+        switch(step) {
+          case 1:
+            step = hg_step1(step, cost);
+            break;
+          case 2:
+            step = hg_step2(step, cost, mask, rowCover, colCover);
+            break;
+          case 3:
+            step = hg_step3(step, mask, colCover);
+            break;
+          case 4:
+            step = hg_step4(step, cost, mask, rowCover, colCover, zero_RC);
+            break;
+          case 5:
+            step = hg_step5(step, mask, rowCover, colCover, zero_RC, path);
+            break;
+          case 6:
+            step = hg_step6(step, cost, rowCover, colCover);
+            break;
+          case 7:
+            done = true;
+            break;
+        }
+      }
+
+      for(i=0; i<mask.length; i++) {
+        for(j=0; j<mask[i].length; j++) {
+          if(mask[i][j] === 1) {
+            assignment[i][0] = i;
+            assignment[i][1] = j;
+          }
+        }
+      }
+
+      // If you want to return the min or max sum instead of the assignment
+      // array, use the following code (or use this code on the return value)
+      // outside of this function):
+
+      var sum = 0;
+      for(i=0; i<assignment.length; i++) {
+        sum = sum + matrix[assignment[i][0]][assignment[i][1]];
+      }
+      return sum;
+
+      // return assignment;
+    }
+
+    function hg_step1(step, cost) {
+      // For each row of the cost matrix, find the smallest element and
+      // subtract it from every other element in its row.
+
+      var minVal, i, j;
+
+      for(i=0; i<cost.length; i++) {
+        minVal = cost[i][0];
+        for(j=0; j<cost[i].length; j++) {
+          if(minVal > cost[i][j]) {
+            minVal = cost[i][j];
+          }
+        }
+        for(j=0; j<cost[i].length; j++) {
+          cost[i][j] = cost[i][j] - minVal;
+        }
+      }
+
+      step = 2;
+      return step;
+    }
+
+    function hg_step2(step, cost, mask, rowCover, colCover) {
+      // Marks uncovered zeros as starred and covers their row and column.
+
+      var i, j;
+
+      for(i=0; i<cost.length; i++) {
+        for(j=0; j<cost[i].length; j++) {
+          if(cost[i][j] === 0 && colCover[j] === 0 && rowCover[i] === 0) {
+            mask[i][j] = 1;
+            colCover[j] = 1;
+            rowCover[i] = 1;
+          }
+        }
+      }
+
+      // Reset cover vectors
+      clearCovers(rowCover, colCover);
+
+      step = 3;
+      return step;
+    }
+
+    function hg_step3(step, mask, colCover) {
+      // Cover columns of starred zeros.  Check if all columns are covered.
+
+      var i, j, count;
+
+      // Cover columns of starred zeros
+      for(i=0; i<mask.length; i++) {
+        for(j=0; j<mask[i].length; j++) {
+          if(mask[i][j] === 1) {
+            colCover[j] = 1;
+          }
+        }
+      }
+
+      // Check if all columns are covered
+      count = 0;
+      for(j=0; j<colCover.length; j++) {
+        count = count + colCover[j];
+      }
+
+      // Should be cost.length, but okay, because mask has same dimensions
+      if(count >= mask.length) {
+        step = 7;
+      } else {
+        step = 4;
+      }
+
+      return step;
+    }
+
+    function hg_step4(step, cost, mask, rowCover, colCover, zero_RC) {
+      // Find an uncovered zero in cost and prime it (if none, go to Step 6).
+      // Check for star in same row: if yes, cover the row and uncover the
+      // star's column.  Repeat until no uncovered zeros are left and go to
+      // Step 6.  If not, save location of primed zero and go to Step 5.
+
+      var row_col = [0,0], // size: 2, holds row and column of uncovered zero
+        done = false,
+        j, starInRow;
+
+      while(!done) {
+        row_col = findUncoveredZero(row_col, cost, rowCover, colCover);
+        if(row_col[0] === -1) {
+          done = true;
+          step = 6;
+        } else {
+          // Prime the found uncovered zero
+          mask[row_col[0]][row_col[1]] = 2;
+
+          starInRow = false;
+          for(j=0; j<mask[row_col[0]].length; j++) {
+            // If there is a star in the same row...
+            if(mask[row_col[0]][j] === 1) {
+              starInRow = true;
+              // Remember its column
+              row_col[1] = j;
+            }
+          }
+
+          if(starInRow) {
+            rowCover[row_col[0]] = 1; // Cover the star's row
+            colCover[row_col[1]] = 0; // Uncover its column
+          } else {
+            zero_RC[0] = row_col[0]; // Save row of primed zero
+            zero_RC[1] = row_col[1]; // Save column of primed zero
+            done = true;
+            step = 5;
+          }
+        }
+      }
+
+      return step;
+    }
+
+    // Auxiliary function for hg_step4
+    function findUncoveredZero(row_col, cost, rowCover, colCover) {
+      var i, j, done;
+
+      row_col[0] = -1; // Just a check value.  Not a real index.
+      row_col[1] = 0;
+
+      i = 0;
+      done = false;
+
+      while(!done) {
+        j = 0;
+        while(j < cost[i].length) {
+          if(cost[i][j] === 0 && rowCover[i] === 0 && colCover[j] === 0) {
+            row_col[0] = i;
+            row_col[1] = j;
+            done = true;
+          }
+          j = j+1;
+        }
+        i++;
+        if(i >= cost.length) {
+          done = true;
+        }
+      }
+
+      return row_col;
+    }
+
+    function hg_step5(step, mask, rowCover, colCover, zero_RC, path) {
+      // Construct series of alternating primes and stars.  Start with prime
+      // from step 4.  Take star in the same column.  Next, take prime in the
+      // same row as the star.  Finish at a prime with no star in its column.
+      // Unstar all stars and star the primes of the series.  Erase any other
+      // primes.  Reset covers.  Go to Step 3.
+
+      var count, done, r, c;
+
+      count = 0; // Counts rows of the path matrix
+      path[count][0] = zero_RC[0]; // Row of last prime
+      path[count][1] = zero_RC[1]; // Column of last prime
+
+      done = false;
+      while(!done) {
+        r = findStarInCol(mask, path[count][1]);
+        if(r >= 0) {
+          count = count+1;
+          path[count][0] = r; // Row of starred zero
+          path[count][1] = path[count-1][1]; // Column of starred zero
+        } else {
+          done = true;
+        }
+
+        if(!done) {
+          c = findPrimeInRow(mask, path[count][0]);
+          count = count+1;
+          path[count][0] = path[count-1][0]; // Row of primed zero
+          path[count][1] = c;
+        }
+      }
+
+      convertPath(mask, path, count);
+      clearCovers(rowCover, colCover);
+      erasePrimes(mask);
+
+      step = 3;
+      return step;
+    }
+
+    // Auxiliary function for hg_step5
+    function findStarInCol(mask, col) {
+      var r, i;
+
+      // Again, this is a check value
+      r = -1;
+      for(i=0; i<mask.length; i++) {
+        if(mask[i][col] === 1) {
+          r = i;
+        }
+      }
+
+      return r;
+    }
+
+    // Auxiliary function for hg_step5
+    function findPrimeInRow(mask, row) {
+      var c, j;
+
+      c = -1;
+      for(j=0; j<mask[row].length; j++) {
+        if(mask[row][j] === 2) {
+          c = j;
+        }
+      }
+
+      return c;
+    }
+
+    // Auxiliary function for hg_step5
+    function convertPath(mask, path, count) {
+      var i;
+
+      for(i=0; i<=count; i++) {
+        if (mask[path[i][0]][path[i][1]] === 1) {
+          mask[path[i][0]][path[i][1]] = 0;
+        } else {
+          mask[path[i][0]][path[i][1]] = 1;
+        }
+      }
+    }
+
+    // Auxiliary function for hg_step5
+    function erasePrimes(mask) {
+      var i, j;
+
+      for(i=0; i<mask.length; i++) {
+        for(j=0; j<mask[i].length; j++) {
+          if(mask[i][j] === 2) {
+            mask[i][j] = 0;
+          }
+        }
+      }
+    }
+
+    // Auxiliary function for hg_step5 (and others)
+    function clearCovers(rowCover, colCover) {
+      var i, j;
+
+      for(i=0; i<rowCover.length; i++) {
+        rowCover[i] = 0;
+      }
+      for(j=0; j<colCover.length; j++) {
+        colCover[j] = 0;
+      }
+    }
+
+    function hg_step6(step, cost, rowCover, colCover) {
+      // Find smallest uncovered value in cost: a.) Add it to every element of
+      // uncovered rows, b.) Subtract it from every element of uncovered
+      // columns.  Go to Step 4.
+
+      var minVal, i, j;
+
+      minVal = findSmallest(cost, rowCover, colCover);
+
+      for(i=0; i<rowCover.length; i++) {
+        for(j=0; j<colCover.length; j++) {
+          if(rowCover[i] === 1) {
+            cost[i][j] += minVal;
+          }
+          if(colCover[j] === 0) {
+            cost[i][j] -= minVal;
+          }
+        }
+      }
+
+      step = 4;
+      return step;
+    }
+
+    // Auxiliary function for hg_step6
+    function findSmallest(cost, rowCover, colCover) {
+      var minVal, i, j;
+
+      // There cannot be a larger cost than this
+      minVal = Number.MAX_VALUE;
+      // Now, find the smallest uncovered value
+      for(i=0; i<cost.length; i++) {
+        for(j=0; j<cost[i].length; j++) {
+          if(rowCover[i] === 0 && colCover[j] === 0 && minVal > cost[i][j]) {
+            minVal = cost[i][j];
+          }
+        }
+      }
+
+      return minVal;
+    }
+
+    // Expose just the hgAlgorithm method
+    // Example call: hungarian(matrix)
+    // Or pass hungarian(matrix, "max") if it's a profit matrix instead of cost
+    return hgAlgorithm;
+  })(this);
+
 })(this);
